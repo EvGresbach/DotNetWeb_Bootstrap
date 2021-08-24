c864984962542d4d5e34181e74aa649b26f9cfd4
Evelynn Gresbach <egresbach1@my.wctc.edu>
2021-08-24 14:34:31 -0500

    Display product name and code

---
+++ js/client.js
@@ -0,0 +1,26 @@
+$(function(){
+    // preload audio
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function(e) {
+
+        e.preventDefault();
+        let code = $(this).data('code');
+        var codeReplaced = $('#code').html().replace('CODE', code); 
+        let productName = $(this).data('productname'); 
+        var productNameReplaced = $('#product').html().replace('Product Name', productName); 
+        $('#code').html(codeReplaced); 
+        $('#product').html(productNameReplaced); 
+
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+
+        
+        
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+});
